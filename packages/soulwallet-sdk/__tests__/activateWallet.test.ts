23ba8ab665042f2dd4304196d98279fe3377ed43
Jayden <z.cejay@gmail.com>
2024-04-17 19:18:21 +0800

    Added P256 testcase

--- packages/soulwallet-sdk/__tests__/activateWallet.test.ts
+++ packages/soulwallet-sdk/__tests__/activateWallet.test.ts
@@ -37,6 +37,8 @@ describe('ActivateWallet', () => {
             return;
         }
         const RPC = "https://sepolia.optimism.io";
+        let signkeyType: SignkeyType;
+        signkeyType = SignkeyType.P256;
         const pimlicoAPIKEY = "";
         const BundlerRPC = `https://api-dev.soulwallet.io/walletapi/bundler/optimism-sepolia/rpc`;
         const pimlicoSponsorRPC = `https://api.pimlico.io/v2/optimism/rpc?apikey=${pimlicoAPIKEY}`;
@@ -60,8 +62,20 @@ describe('ActivateWallet', () => {
         //const signer = ethers.Wallet.createRandom();
         const signer = new ethers.Wallet('0x0000000000000000000000000000000000000000000000000000000000000001');
 
+        // new webauthn signer
+        const { privateKeyBase64, publicKeyBase64, X, Y } = await WebAuthNMock.createPassKey();
+        const p256KeyHash = WebAuthN.publicKeyToKeyhash({
+            x: WebAuthNMock.paddingZero(X, 32),
+            y: WebAuthNMock.paddingZero(Y, 32)
+        });
+
         const index: number = 0;
-        const initialKeys: InitialKey[] = [signer.address];
+        let initialKeys: InitialKey[] = [];
+        if (signkeyType.valueOf() === SignkeyType.EOA.valueOf()) {
+            initialKeys.push(signer.address);
+        } else {
+            initialKeys.push(p256KeyHash);
+        }
         const initialGuardianHash: string = "0x0000000000000000000000000000000000000000000000000000000000000001";
 
         const _walletAddress = await soulWallet.calcWalletAddress(
@@ -96,7 +110,7 @@ describe('ActivateWallet', () => {
         }
 
         {
-            const re = await soulWallet.estimateUserOperationGas(SoulWalletDefaultValidator, userOp, SignkeyType.EOA);
+            const re = await soulWallet.estimateUserOperationGas(SoulWalletDefaultValidator, userOp, signkeyType);
             if (userOp.callData === '0x') {
                 userOp.callGasLimit = 1;
             }
@@ -111,7 +125,7 @@ describe('ActivateWallet', () => {
             userOp.paymasterVerificationGasLimit = 0;
             userOp.paymasterPostOpGasLimit = 0;
             userOp.paymasterData = "0x";
-            userOp.signature = (await soulWallet.getSemiValidSignature(SoulWalletDefaultValidator, userOp, SignkeyType.EOA)).OK;
+            userOp.signature = (await soulWallet.getSemiValidSignature(SoulWalletDefaultValidator, userOp, signkeyType)).OK;
             const usePimlico = true;
             if (usePimlico) {
                 const SponsorRPC = new ethers.JsonRpcProvider(
@@ -213,11 +227,28 @@ describe('ActivateWallet', () => {
             const packedHash: string = _re.OK.packedHash;
             const validationData: string = _re.OK.validationData;
 
-            const _signature = signer.signMessageSync(ethers.getBytes(packedHash));
+            if (signkeyType.valueOf() === SignkeyType.EOA.valueOf()) {
+                const _signature = signer.signMessageSync(ethers.getBytes(packedHash));
+                const signature = await soulWallet.packUserOpEOASignature(SoulWalletDefaultValidator, _signature, validationData);
+                expect(signature.isOk()).toBe(true);
+                userOp.signature = signature.OK;
+            } else {
+                const { r, s, authenticatorData, clientDataSuffix, _message } = await WebAuthNMock.signPassKey(privateKeyBase64, publicKeyBase64, packedHash);
 
-            const signature = await soulWallet.packUserOpEOASignature(SoulWalletDefaultValidator, _signature, validationData);
-            expect(signature.isOk()).toBe(true);
-            userOp.signature = signature.OK;
+                const signature = await soulWallet.packUserOpP256Signature(SoulWalletDefaultValidator, {
+                    messageHash: packedHash,
+                    publicKey: {
+                        x: WebAuthNMock.paddingZero(X, 32),
+                        y: WebAuthNMock.paddingZero(Y, 32)
+                    },
+                    r: WebAuthNMock.paddingZero(r, 32),
+                    s: WebAuthNMock.paddingZero(s, 32),
+                    authenticatorData: authenticatorData,
+                    clientDataSuffix: clientDataSuffix
+                }, validationData);
+                expect(signature.isOk()).toBe(true);
+                userOp.signature = signature.OK;
+            }
         }
         {
             const packedUserOp = UserOpUtils.packUserOp(userOp);
@@ -241,6 +272,188 @@ describe('ActivateWallet', () => {
 
 
 
-    }, 1000 * 60);
+    }, 1000 * 60 * 10);
 });
+
+
+
+class WebAuthNMock {
+
+    public static paddingZero(value: string | number | bigint, bytesLen?: number): string {
+        if (typeof value === 'string') {
+            if (value.startsWith('0x')) {
+                value = value.slice(2);
+            }
+            let len = 0;
+            if (bytesLen === undefined) {
+                len = value.length + (value.length % 2);
+            } else {
+                len = bytesLen * 2;
+            }
+            if (value.length > len) {
+                throw new Error(`value ${value} length is greater than ${len}`);
+            }
+            return '0x' + '0'.repeat(len - value.length) + value.toLowerCase();
+        }
+        else if (typeof value === 'number' || typeof value === 'bigint') {
+            return this.paddingZero(value.toString(16), bytesLen);
+        } else {
+            throw new Error(`value ${value} is not string | number | bigint`);
+        }
+    }
+
+    /**
+   * base64Url
+   *
+   * @static
+   * @param {string} data
+   * @return {*} 
+   * @memberof Base64Url
+   */
+    private static base64Url(data: string) {
+        return btoa(data).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+    }
+
+    /**
+     * base64UrlToBytes32
+     *
+     * @static
+     * @param {string} bytes32Str
+     * @return {*} 
+     * @memberof Base64Url
+     */
+    private static bytes32Tobase64Url(bytes32Str: string) {
+        const userOpHashForBytes = bytes32Str.startsWith('0x') ? bytes32Str.slice(2) : bytes32Str;
+        const byteArray = new Uint8Array(32);
+        for (let i = 0; i < 64; i += 2) {
+            byteArray[i / 2] = parseInt(userOpHashForBytes.substring(i, i + 2), 16);
+        }
+        return WebAuthNMock.base64Url(String.fromCharCode(...byteArray));
+    }
+
+    private static uint8ArrayToHex(uint8Array: Uint8Array): string {
+        return '0x' + Array.from(uint8Array).map(b => b.toString(16).padStart(2, '0')).join('');
+    }
+
+    private static hexToUint8Array(hex: string): Uint8Array {
+        if (hex.startsWith('0x')) hex = hex.slice(2);
+        const len = hex.length;
+        const uint8Array = new Uint8Array(len / 2);
+        for (let i = 0; i < len; i += 2) {
+            uint8Array[i / 2] = parseInt(hex.substring(i, i + 2), 16);
+        }
+        return uint8Array;
+    }
+
+    private static arrayBufferToBase64(buffer: ArrayBuffer) {
+        let binary = '';
+        const bytes = new Uint8Array(buffer);
+        for (let i = 0; i < bytes.byteLength; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return Buffer.from(binary, 'binary').toString('base64');
+    }
+    private static base64ToArrayBuffer(base64: string) {
+        const binary_string = Buffer.from(base64, 'base64').toString('binary');
+        const len = binary_string.length;
+        const bytes = new Uint8Array(len);
+        for (let i = 0; i < len; i++) {
+            bytes[i] = binary_string.charCodeAt(i);
+        }
+        return bytes.buffer;
+    }
+
+    public static async createPassKey() {
+        const algoParams = {
+            name: 'ECDSA',
+            namedCurve: 'P-256',
+            hash: 'SHA-256'
+        };
+        const keyPair = await webcrypto.subtle.generateKey(algoParams, true, ['sign', 'verify']);
+        let X: bigint;
+        let Y: bigint;
+        // keyPair.publicKey to x,y
+        const publicKeyBuffer = await webcrypto.subtle.exportKey('spki', keyPair.publicKey);
+        if (publicKeyBuffer.byteLength === 91) {
+            // ASN.1 DER format
+            const _buffer: Uint8Array = new Uint8Array(publicKeyBuffer);
+            if (_buffer[26] !== 0x04) {
+                throw new Error('Unexpected public key format');
+            }
+            const x = publicKeyBuffer.slice(27, 59);
+            const y = publicKeyBuffer.slice(59, 91);
+            X = BigInt(WebAuthNMock.uint8ArrayToHex(new Uint8Array(x)));
+            Y = BigInt(WebAuthNMock.uint8ArrayToHex(new Uint8Array(y)));
+        } else {
+            throw new Error('Unexpected public key format');
+        }
+        const privateKeyBase64 = WebAuthNMock.arrayBufferToBase64(await webcrypto.subtle.exportKey('pkcs8', keyPair.privateKey));
+        const publicKeyBase64 = WebAuthNMock.arrayBufferToBase64(await webcrypto.subtle.exportKey('spki', keyPair.publicKey));
+
+        return {
+            privateKeyBase64,
+            publicKeyBase64,
+            X,
+            Y
+        };
+    }
+
+    public static async signPassKey(privateKeyBase64: string, publicKeyBase64: string, userOpHash: string) {
+
+        const algoParams = {
+            name: 'ECDSA',
+            namedCurve: 'P-256',
+            hash: 'SHA-256'
+        };
+
+        const publicKey: webcrypto.CryptoKey = await webcrypto.subtle.importKey(
+            'spki',
+            WebAuthNMock.base64ToArrayBuffer(publicKeyBase64),
+            algoParams,
+            true,
+            ['verify']
+        );
+        const privateKey: webcrypto.CryptoKey = await webcrypto.subtle.importKey(
+            'pkcs8',
+            WebAuthNMock.base64ToArrayBuffer(privateKeyBase64),
+            algoParams,
+            true,
+            ['sign']
+        );
+        const keyPair = {
+            publicKey,
+            privateKey
+        };
+        const challengeBase64 = WebAuthNMock.bytes32Tobase64Url(userOpHash);
+        const clientDataSuffix = '","origin":"https://webauthn-mock.soulwallet.io","crossOrigin":false}';
+        const clientDataJSON = `{"type":"webauthn.get","challenge":"${challengeBase64}${clientDataSuffix}`;
+        const jsonBytes = ethers.toUtf8Bytes(clientDataJSON);
+        const jsonBytesHex = '0x' + Array.from(jsonBytes).map(b => b.toString(16).padStart(2, '0')).join('');
+        let clientDataJSONHash: string = ethers.sha256(jsonBytesHex);
+        let authenticatorData = '0x49960de5880e8c687434170f6476605b8fe4aeb9a28632c7995cf3ba831d97630500000000';
+        if (clientDataJSONHash.startsWith('0x')) clientDataJSONHash = clientDataJSONHash.slice(2);
+        const message = authenticatorData + clientDataJSONHash;
+        const _message = ethers.sha256(message);
+
+        const signature = await webcrypto.subtle.sign(
+            algoParams,
+            keyPair.privateKey,
+            Buffer.from(message.slice(2), 'hex')
+        );
+        const isValid = await webcrypto.subtle.verify(algoParams, keyPair.publicKey, signature, Buffer.from(message.slice(2), 'hex'));
+        if (isValid !== true) {
+            throw new Error('sign failed');
+        }
+
+        const r = WebAuthNMock.uint8ArrayToHex(new Uint8Array(signature.slice(0, 32)));
+        const s = WebAuthNMock.uint8ArrayToHex(new Uint8Array(signature.slice(32, 64)));
+        return {
+            r,
+            s,
+            authenticatorData,
+            clientDataSuffix,
+            _message
+        }
+    }
+}
 
